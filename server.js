bfb8f51aaf2a58f5df149bb073e250ad5f391e40
Minh Anh Trinh <minhanh.xiu@gmail.com>
2018-03-13 14:30:43 -0400

    heroku config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -56,7 +56,8 @@ app.get('/bad', (req, res) => {
   });
 });
 
-// Telling the app to listen to port 3000 & log out a message on success
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+// Telling the app to listen to port (either heroku port or port localhost:3000)
+// & log out a message on success.
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
